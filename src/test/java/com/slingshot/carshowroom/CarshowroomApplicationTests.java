76472fb1e56898d435665e4a465409a7a8746757
jasmynejean-remy <jasmynej14@gmail.com>
2026-04-20 16:04:09 -0400

    init

---
+++ src/test/java/com/slingshot/carshowroom/CarshowroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slingshot.carshowroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshowroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
